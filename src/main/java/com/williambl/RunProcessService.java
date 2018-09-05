51c5d43d4256bb7c43f2a5e66fe56a9638d283c7
William Bradford Larcombe <william@williambl.com>
2018-09-05 11:34:58 +0100

    Fix Indentation

--- src/main/java/com/williambl/RunProcessService.java
+++ src/main/java/com/williambl/RunProcessService.java
@@ -12,50 +12,50 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 @XStreamAlias("run-process-service")
 public class RunProcessService extends ServiceImp {
 
-  @NotBlank
-  private String command;
+    @NotBlank
+    private String command;
 
-  @NotBlank
-  private String directory;
+    @NotBlank
+    private String directory;
 
-  public void doService(AdaptrisMessage msg) throws ServiceException {
+    public void doService(AdaptrisMessage msg) throws ServiceException {
 
-      // This scary-looking regex will split the string on spaces
-      // unless that space is escaped by a backslash.
-      String[] args = command.split("(?<=(?<!\\\\)(\\\\\\\\){0,100}) ");
+        // This scary-looking regex will split the string on spaces
+        // unless that space is escaped by a backslash.
+        String[] args = command.split("(?<=(?<!\\\\)(\\\\\\\\){0,100}) ");
 
-      ProcessBuilder pb = new ProcessBuilder(Arrays.asList(args));
-      pb.directory(new File(directory));
-      try {
-      Process p = pb.start();
-      } catch (IOException e) {
-        System.out.println(e.getStackTrace());
-      }
-  }
+        ProcessBuilder pb = new ProcessBuilder(Arrays.asList(args));
+        pb.directory(new File(directory));
+        try {
+            Process p = pb.start();
+        } catch (IOException e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
 
-  public final void prepare() {
-  }
+    public final void prepare() {
+    }
 
-  public final void initService() {
-  }
+    public final void initService() {
+    }
 
-  public final void closeService() {
-  }
+    public final void closeService() {
+    }
 
-  public void setCommand (String commandIn) {
-    this.command = commandIn;
-  }
+    public void setCommand (String commandIn) {
+        this.command = commandIn;
+    }
 
-  public String getCommand () {
-    return this.command;
-  }
-  
-  public void setDirectory (String directoryIn) {
-    this.directory = directoryIn;
-  }
+    public String getCommand () {
+        return this.command;
+    }
 
-  public String getDirectory () {
-    return this.directory;
-  }
+    public void setDirectory (String directoryIn) {
+        this.directory = directoryIn;
+    }
+
+    public String getDirectory () {
+        return this.directory;
+    }
 
 }
--- src/test/java/com/williambl/RunProcessServiceTest.java
+++ src/test/java/com/williambl/RunProcessServiceTest.java
@@ -4,27 +4,27 @@ import com.adaptris.core.*;
 
 public class RunProcessServiceTest extends ServiceCase {
 
-  public RunProcessServiceTest(java.lang.String testName) {
-    super(testName);
-  }
+    public RunProcessServiceTest(java.lang.String testName) {
+        super(testName);
+    }
 
-  @Override
-  protected void setUp() throws Exception {
-  }
+    @Override
+    protected void setUp() throws Exception {
+    }
 
-  public void testService() throws Exception {
-    RunProcessService service = new RunProcessService();
-    service.setCommand("ls");
-    service.setDirectory("/home/william/dev/Interlok/bin/Interlok");
-    execute(service, AdaptrisMessageFactory.getDefaultInstance().newMessage());
-  }
+    public void testService() throws Exception {
+        RunProcessService service = new RunProcessService();
+        service.setCommand("ls");
+        service.setDirectory("/home/william/dev/Interlok/bin/Interlok");
+        execute(service, AdaptrisMessageFactory.getDefaultInstance().newMessage());
+    }
 
-  @Override
-  protected Object retrieveObjectForSampleConfig() {
-    RunProcessService service = new RunProcessService();
-    service.setCommand("ls");
-    service.setDirectory("/home/william/dev/Interlok/bin/Interlok");
-    return service;
-  }
+    @Override
+    protected Object retrieveObjectForSampleConfig() {
+        RunProcessService service = new RunProcessService();
+        service.setCommand("ls");
+        service.setDirectory("/home/william/dev/Interlok/bin/Interlok");
+        return service;
+    }
 
 }
